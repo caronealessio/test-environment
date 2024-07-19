62378dac2470980304418e3af026253a6babd55a
Alessio Pietro Carone <caronealessio99@gmail.com>
2024-07-19 12:50:21 +0200

    dev: terminato currency

--- webapp/controls/Currency.js
+++ webapp/controls/Currency.js
@@ -19,6 +19,18 @@ sap.ui.define(["sap/m/Input", "sap/m/Label", "sap/ui/core/format/NumberFormat"],
           type: "int",
           defaultValue: 2,
         },
+        textAlign: {
+          type: "string",
+          defaultValue: "End",
+        },
+        maxLength: {
+          type: "string",
+          defaultValue: "26",
+        },
+        enableNull: {
+          type: "boolean",
+          defaultValue: false,
+        },
       },
       aggregations: {
         _label: { type: "sap.m.Label", multiple: false, visibility: "hidden" },
@@ -41,34 +53,36 @@ sap.ui.define(["sap/m/Input", "sap/m/Label", "sap/ui/core/format/NumberFormat"],
     },
 
     onChange: function (oEvent) {
+      var oCurrencyFormat = NumberFormat.getCurrencyInstance();
       var sValue = this.getValue();
       var sModel = oEvent.srcControl.getBindingInfo("value").parts[0].model;
       var sPath = oEvent.srcControl.getBindingPath("value");
 
+      //Formatto il valore cosicchè ci possiamo lavorare
       sValue = sValue.replace(".", "");
       sValue = sValue.replace(",", ".");
 
-      var aValue = sValue.split(".");
-
-      var iIntegerDigits = aValue[0].length;
-      var iMaxIntegerDigits = this.getMaxIntegerDigits();
-      var iDecimalDigits = aValue[1].length;
-      var iMaxDecimalDigits = this.getMaxDecimalDigits();
-
-      this.setValueState(sap.ui.core.ValueState.None);
-
-      if (
-        (iIntegerDigits > iMaxIntegerDigits && iMaxIntegerDigits) ||
-        (iDecimalDigits > iMaxDecimalDigits && iMaxDecimalDigits)
-      ) {
-        this.setValueState(sap.ui.core.ValueState.Error);
-
-        sValue = this.getModel(sModel).getProperty(sPath);
-
-        var oCurrencyFormat = NumberFormat.getCurrencyInstance();
-        this.setValue(oCurrencyFormat.format(parseFloat(sValue)));
+      //Se è abilitato a null una volta cancellato non viene messo in automatico a 0,00
+      if (this.getEnableNull() && !sValue) {
+        // this.setValue(null);
+        this.getModel(sModel).setProperty(sPath, null);
+        return;
       }
 
+      //Controllo la lunghezza degli interi e dei decimali
+      if (!this._checkLength(sValue)) {
+        this.setValueState(sap.ui.core.ValueState.Error);
+        sValue = this.getModel(sModel).getProperty(sPath);
+        this.setValue(oCurrencyFormat.format(sValue));
+      }
+
+      //Controllo se è stato inserito 0
+      if (oCurrencyFormat.format(parseFloat(sValue)) === "0,00") {
+        sValue = "0.00";
+        this.setValue(oCurrencyFormat.format(sValue));
+      }
+
+      this.setValue(oCurrencyFormat.format(sValue));
       this.getModel(sModel).setProperty(sPath, sValue);
     },
 
@@ -90,6 +104,10 @@ sap.ui.define(["sap/m/Input", "sap/m/Label", "sap/ui/core/format/NumberFormat"],
       return this.getProperty("label");
     },
 
+    getEnableNull: function () {
+      return this.getProperty("enableNull");
+    },
+
     _createLabel: function (oRm, oInput) {
       if (!oInput.getAggregation("_label").getText() && oInput.getLabel()) {
         oInput.getAggregation("_label").setVisible(oInput.getLabel() ? true : false);
@@ -97,6 +115,26 @@ sap.ui.define(["sap/m/Input", "sap/m/Label", "sap/ui/core/format/NumberFormat"],
 
         oRm.renderControl(oInput.getAggregation("_label"));
       }
+    },
+
+    _checkLength: function (sValue) {
+      var aValue = sValue.split(".");
+
+      var iIntegerDigits = aValue[0]?.length;
+      var iMaxIntegerDigits = this.getMaxIntegerDigits();
+      var iDecimalDigits = aValue[1]?.length;
+      var iMaxDecimalDigits = this.getMaxDecimalDigits();
+
+      this.setValueState(sap.ui.core.ValueState.None);
+
+      if (
+        (iIntegerDigits > iMaxIntegerDigits && iMaxIntegerDigits) ||
+        (iDecimalDigits > iMaxDecimalDigits && iMaxDecimalDigits)
+      ) {
+        return false;
+      }
+
+      return true;
     },
   });
 });

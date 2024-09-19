230fa626b66aaa9886e70c780960b3be734419a2
Alessio Pietro Carone <caronealessio99@gmail.com>
2024-09-19 16:36:48 +0200

    dev: aggiunta libreria moment + aggiunto pulsante di test generale

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -5,6 +5,7 @@ sap.ui.define(
     "testenvironment/model/mockdata",
     "testenvironment/controller/functionality/StandardTableManagement/library.controller",
     "testenvironment/model/formatter",
+    "testenvironment/libs/moment",
   ],
 
   function (BaseController, JSONModel, mockdata, StandardTableManagement, formatter) {
@@ -24,6 +25,10 @@ sap.ui.define(
         var sRoute = oEvent.getParameter("item").getProperty("key");
 
         this.getRouter().navTo(sRoute);
+      },
+
+      onTest: function (oEvent) {
+        console.log(moment().format("YYYY-MM-DD[T]HH:mm:ss.SSS[Z]"));
       },
     });
   }
